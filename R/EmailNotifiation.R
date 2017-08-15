8fd1f28946c446a7f6ba34fd98c491157efc799b
Richard White <w@rwhite.no>
2017-08-15 13:32:38 +0300

    EmailNotificationKommuneLeger

--- R/EmailNotifiation.R
+++ R/EmailNotifiation.R
@@ -66,8 +66,42 @@ EmailNotificationUtbrudd <- function(lastEmailedUtbruddFile=fhi::DashboardFolder
                             emailText)
 
     CheckForOutbreaksUtbrudd()
+    EmailNotificationKommuneLeger()
   }
   saveRDS(thisWeek,file=lastEmailedUtbruddFile)
+}
+
+#' test
+#' @import fhi
+#' @export EmailNotificationKommuneLeger
+EmailNotificationKommuneLeger <- function(){
+
+  emailText <- "
+  Innlogging:<br>
+  Webadresse: <a href='http://sykdomspulsen.fhi.no/lege123/'>http://sykdomspulsen.fhi.no/lege123/</a><br>
+  Det er ikke noe brukernavn eller passord, du kommer direkte inn p\u00E5 nettsiden og den er klar til bruk.<br>
+  NB! Bruk Google Chrome n\u00E5r du logger deg inn!<br>
+  NB! Dette er et pilotprosjekt. Du f\u00E5r n\u00E5 mulighet til \u00E5 bruke websiden n\u00E5r du vil og s\u00E5 mye du vil.<br>
+  Du kan ogs\u00E5 vise enkeltsider av websiden til andre som jobber innenfor kommunehelsetjenesten.<br>
+  MEN vi ber om at du ikke distribuerer webadressen til andre, hverken til ansatte i kommunehelsetjenesten eller utenfor.<br>
+  Det er fordi dette er et pilotprosjekt der vi \u00F8nsker \u00E5 ha oversikt over hvem som bruker systemet.<br>
+  Dersom noen andre enn deg \u00F8nsker \u00E5 f\u00E5 tilgang til websiden kan de kontakte oss p\u00E5 sykdomspulsen@fhi.no
+  <br><br>
+  Vi \u00F8nsker tilbakemeldinger!
+  <br><br>
+  Dersom du har problemer med websiden, forslag til forbedringer, ris eller ros kan du sende oss en mail: sykdomspulsen@fhi.no<br>
+  Dersom du ikke \u00F8nsker \u00E5 f\u00E5 denne e-posten n\u00E5r vi oppdaterer Sykdomspulsen med nye tall s\u00E5 kan du gi oss beskjed ved \u00E5 sende en mail til adressen over.
+  <br><br>
+  Hilsen:<br>
+  Sykdomspulsen ved Folkehelseinstituttet<br>
+  v/Gry M Gr\u00F8neng (prosjektleder) og Richard White (statistiker og webansvarlig)
+  "
+
+  if(Sys.getenv("COMPUTER")=="smhb"){
+    fhi::DashboardEmail("sykdomspuls_kommuneleger",
+                        "Pilotprosjektet Sykdomspulsen til kommunehelsetjenesten er n\u00E5 oppdatert med nye tall",
+                        emailText)
+  }
 }
 
 #' test
