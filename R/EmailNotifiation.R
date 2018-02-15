9be28e4c5393a65c580e4dabb4f217f5563963c3
Richard White <r@rwhite.no>
2018-02-15 14:30:21 +0100

    Fixing emails to kommuneleger

--- R/EmailNotifiation.R
+++ R/EmailNotifiation.R
@@ -70,66 +70,20 @@ EmailNotificationUtbrudd <- function(lastEmailedUtbruddFile=fhi::DashboardFolder
     #                        emailText)
 
     try(CheckForOutbreaksUtbrudd(),TRUE)
-    try(EmailNotificationKommuneLeger(),TRUE)
     try(EmailAlertExternal(),TRUE)
   }
   saveRDS(thisWeek,file=lastEmailedUtbruddFile)
 }
 
 #' test
-#' @import fhi
-#' @export EmailNotificationKommuneLeger
-EmailNotificationKommuneLeger <- function(){
-
-  emailText <- "
-  Pilotprosjektet Sykdomspulsen til kommunehelsetjenesten er oppdatert med nye tall<br>
-  Nye resultater vises p\u00E5 websiden om ca. 10 min.<br><br>
-  Innlogging:<br>
-  Webadresse: <a href='http://sykdomspulsen.fhi.no/lege123/'>http://sykdomspulsen.fhi.no/lege123/</a><br>
-  Det er ikke noe brukernavn eller passord, du kommer direkte inn p\u00E5 nettsiden og den er klar til bruk.<br>
-  Bruk Google Chrome n\u00E5r du logger deg inn.<br><br>
-  NB! Dette er et pilotprosjekt. Du f\u00E5r n\u00E5 mulighet til \u00E5 bruke websiden n\u00E5r du vil og s\u00E5 mye du vil.<br>
-  Du kan ogs\u00E5 vise enkeltsider av websiden til andre som jobber innenfor kommunehelsetjenesten.<br>
-  MEN vi ber om at du ikke distribuerer webadressen til andre, hverken til ansatte i kommunehelsetjenesten eller utenfor.<br>
-  Det er fordi dette er et pilotprosjekt der vi \u00F8nsker \u00E5 ha oversikt over hvem som bruker systemet.<br>
-  Dersom noen andre enn deg \u00F8nsker \u00E5 f\u00E5 tilgang til websiden kan de kontakte oss p\u00E5 sykdomspulsen@fhi.no
-  <br><br>
-  Vi \u00F8nsker tilbakemeldinger!
-  <br><br>
-  Dersom du har problemer med websiden, forslag til forbedringer, ris eller ros kan du sende oss en mail: sykdomspulsen@fhi.no<br>
-  Dersom du ikke \u00F8nsker \u00E5 f\u00E5 denne e-posten n\u00E5r vi oppdaterer Sykdomspulsen med nye tall s\u00E5 kan du gi oss beskjed ved \u00E5 sende en mail til adressen over.
-  <br><br>
-  <b>Ny funksjon for oversikt-siden</b><br>
-  Etter tilbakemelding fra en av pilotbrukerne har vi forbedret funksjonen p\u00E5 oversikt-siden.
-  N\u00E5 er det mulig \u00E5 klikke p\u00E5 feltene i diagrammet p\u00E5 oversikt-siden.
-  Du vil da komme direkte til ukentlig-siden der grafen vil vise samme sykdom/symptom, kommune og aldersgruppe som du klikket p\u00E5.
-  <br><br>
-  OBS: Nord og S\u00F8r-Tr\u00F8ndelag har fra 01.01.2018 blitt sl\u00E5tt sammen til Tr\u00F8ndelag. Det vil derfor bare v\u00E6re mulig \u00E5 finne Tr\u00F8ndelag i nedtrekkslisten for Fylkene.
-  <br><br>
-  Hilsen:<br>
-  Sykdomspulsen ved Folkehelseinstituttet<br>
-  v/Gry M Gr\u00F8neng (prosjektleder) og Richard White (statistiker og webansvarlig)<br><br><br>
-  "
-
-  if(Sys.getenv("COMPUTER")=="smhb"){
-    fhi::DashboardEmail("sykdomspuls_kommuneleger",
-                        "Pilotprosjektet Sykdomspulsen til kommunehelsetjenesten er oppdatert med nye tall",
-                        emailText,
-                        emailFooter=FALSE)
-  } else if(Sys.getenv("COMPUTER")=="test"){
-    fhi::DashboardEmail("test",
-                        "Pilotprosjektet Sykdomspulsen til kommunehelsetjenesten er oppdatert med nye tall",
-                        emailText,
-                        emailFooter=FALSE)
-  }
-}
-
-#' test
 #' @param results a
+#' @param alerts a
 #' @importFrom RAWmisc Format
 #' @import fhi
 #' @export EmailAlertExternal
-EmailAlertExternal <- function(results=readRDS(fhi::DashboardFolder("results","outbreaks_alert_external.RDS"))){
+EmailAlertExternal <- function(
+  results=readRDS(fhi::DashboardFolder("results","outbreaks_alert_external.RDS")),
+  alerts = readxl::read_excel(file.path("/etc", "gmailr", "emails_sykdomspuls_alert.xlsx"))){
   # variables used in data.table functions in this function
   output <- NULL
   type <- NULL
@@ -139,69 +93,116 @@ EmailAlertExternal <- function(results=readRDS(fhi::DashboardFolder("results","o
   cumE1 <- NULL
   zscore <- NULL
   email <- NULL
+  link <- NULL
+  county <- NULL
   # end
 
-  if(nrow(results)==0){
-    return(0)
+  if(Sys.getenv("COMPUTER")!="smhb"){
+    alerts <- readxl::read_excel(file.path("/etc", "gmailr", "emails_sykdomspuls_alert_test.xlsx"))
   }
-  emails <- unique(results$email)
+
+  setDT(alerts)
+  emails <- unique(alerts$email)
 
   emailHeader <-
     "<style>
-html {
-  font-family: sans-serif;
-}
+    html {
+      font-family: sans-serif;
+    }
 
-table {
-  border-collapse: collapse;
-  border: 2px solid rgb(200,200,200);
-  letter-spacing: 1px;
-  font-size: 0.8rem;
-}
+    table {
+      border-collapse: collapse;
+      border: 2px solid rgb(200,200,200);
+      letter-spacing: 1px;
+      font-size: 0.8rem;
+    }
 
-td, th {
-  border: 1px solid rgb(190,190,190);
-  padding: 10px 20px;
-}
+    td, th {
+      border: 1px solid rgb(190,190,190);
+      padding: 10px 20px;
+    }
 
-th {
-  background-color: rgb(235,235,235);
-}
+    th {
+      background-color: rgb(235,235,235);
+    }
 
-td {
-  text-align: center;
-}
+    td {
+      text-align: center;
+    }
 
-tr:nth-child(even) td {
-  background-color: rgb(250,250,250);
-}
+    tr:nth-child(even) td {
+      background-color: rgb(250,250,250);
+    }
 
-tr:nth-child(odd) td {
-  background-color: rgb(245,245,245);
-}
+    tr:nth-child(odd) td {
+      background-color: rgb(245,245,245);
+    }
 
-caption {
-  padding: 10px;
-}
-</style>"
+    caption {
+      padding: 10px;
+    }
+    </style>
 
-  results[,output:=sprintf("<tr> <td>%s</td> <td>%s</td> <td>%s</td> <td>%s</td> <td>%s</td> <td>%s</td> </tr>", type, locationName, location, age, round(cumE1), RAWmisc::Format(zscore,digits=2))]
+    Pilotprosjektet Sykdomspulsen til kommunehelsetjenesten er oppdatert med nye tall<br>
+    Nye resultater vises p\u00E5 websiden om ca. 10 min.<br><br>
+    Innlogging:<br>
+    Webadresse: <a href='http://sykdomspulsen.fhi.no/lege123/'>http://sykdomspulsen.fhi.no/lege123/</a><br>
+    Det er ikke noe brukernavn eller passord, du kommer direkte inn p\u00E5 nettsiden og den er klar til bruk.<br>
+    Bruk Google Chrome n\u00E5r du logger deg inn.<br><br>
+    NB! Dette er et pilotprosjekt. Du f\u00E5r n\u00E5 mulighet til \u00E5 bruke websiden n\u00E5r du vil og s\u00E5 mye du vil.<br>
+    Du kan ogs\u00E5 vise enkeltsider av websiden til andre som jobber innenfor kommunehelsetjenesten.<br>
+    MEN vi ber om at du ikke distribuerer webadressen til andre, hverken til ansatte i kommunehelsetjenesten eller utenfor.<br>
+    Det er fordi dette er et pilotprosjekt der vi \u00F8nsker \u00E5 ha oversikt over hvem som bruker systemet.<br>
+    Dersom noen andre enn deg \u00F8nsker \u00E5 f\u00E5 tilgang til websiden kan de kontakte oss p\u00E5 sykdomspulsen@fhi.no
+    <br><br>
+    Vi \u00F8nsker tilbakemeldinger!
+    <br><br>
+    Dersom du har problemer med websiden, forslag til forbedringer, ris eller ros kan du sende oss en mail: sykdomspulsen@fhi.no<br>
+    Dersom du ikke \u00F8nsker \u00E5 f\u00E5 denne e-posten n\u00E5r vi oppdaterer Sykdomspulsen med nye tall s\u00E5 kan du gi oss beskjed ved \u00E5 sende en mail til adressen over.
+    <br><br>
+    <b>Ny funksjon for oversikt-siden</b><br>
+    Etter tilbakemelding fra en av pilotbrukerne har vi forbedret funksjonen p\u00E5 oversikt-siden.
+    N\u00E5 er det mulig \u00E5 klikke p\u00E5 feltene i diagrammet p\u00E5 oversikt-siden.
+    Du vil da komme direkte til ukentlig-siden der grafen vil vise samme sykdom/symptom, kommune og aldersgruppe som du klikket p\u00E5.
+    <br><br>
+    OBS: Nord og S\u00F8r-Tr\u00F8ndelag har fra 01.01.2018 blitt sl\u00E5tt sammen til Tr\u00F8ndelag. Det vil derfor bare v\u00E6re mulig \u00E5 finne Tr\u00F8ndelag i nedtrekkslisten for Fylkene.
+    <br><br>
+    Hilsen:<br>
+    Sykdomspulsen ved Folkehelseinstituttet<br>
+    v/Gry M Gr\u00F8neng (prosjektleder) og Richard White (statistiker og webansvarlig)<br><br><br>
+    "
+
+  alerts[,output:=sprintf("<tr> <td>%s</td> </tr>", location)]
+
+  results[,link:=sprintf("<a href='http://sykdomspulsen.fhi.no/lege123/#/ukentlig/%s/%s/%s/%s'>Klikk</a>",county,location,type,age)]
+  results[is.na(county),link:=sprintf("<a href='http://sykdomspulsen.fhi.no/lege123/#/ukentlig/%s/%s/%s/%s'>Klikk</a>",location,location,type,age)]
+  results[,output:=sprintf("<tr> <td>%s</td> <td>%s</td> <td>%s</td> <td>%s</td> <td>%s</td> <td>%s</td> <td>%s</td> </tr>", link, type, locationName, location, age, round(cumE1), RAWmisc::Format(zscore,digits=2))]
 
   for(em in emails){
     r <- results[email %in% em]
-    if(nrow(r)==0){
-      next
+    a <- alerts[email %in% em]
+
+    emailText <- paste0(emailHeader,"Registered for outbreaks in:<br><br><table style='width:90%'><tr><th>location</th></tr>")
+    for(i in 1:nrow(a)){
+      emailText <- sprintf("%s%s", emailText, a$output[i])
     }
-    emailText <- paste0(emailHeader,"Outbreaks:<br><br><table style='width:90%'><tr><th>Syndrome</th> <th>Location</th> <th>Location</th> <th>Age</th> <th>Excess</th> <th>Z-score</th></tr>")
-    for(i in 1:nrow(r)){
-      emailText <- sprintf("%s%s", emailText, r$output[i])
-    }
-    emailText <- sprintf("%s</table>", emailText)
+    emailText <- sprintf("%s</table><br><br>", emailText)
+
+    # if(nrow(r)==0){
+    #   emailText <- paste0(emailText,"Outbreaks:<br><br>No outbreaks recorded")
+    # } else {
+    #   emailText <- paste0(emailText,"Outbreaks:<br><br><table style='width:90%'><tr><th>Til nettsiden</th> <th>Syndrome</th> <th>Location</th> <th>Location</th> <th>Age</th> <th>Excess</th> <th>Z-score</th></tr>")
+    #   for(i in 1:nrow(r)){
+    #     emailText <- sprintf("%s%s", emailText, r$output[i])
+    #   }
+    #   emailText <- sprintf("%s</table>", emailText)
+    # }
     fhi::DashboardEmailSpecific(emailBCC = em,
                                 emailSubject = "Outbreaks",
                                 emailText = emailText)
     Sys.sleep(5)
   }
+  return(0)
 }
 
 #' test
